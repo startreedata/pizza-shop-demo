5ce85b3a914e5e1b00e01c27ccd75aa89fc6b8b9
Oli <olga.makhasoeva@gmail.com>
2024-03-04 23:30:21 -0800

    add joins
    added a running named join, transformed data to the expected format

--- enrichment-bytewax/dataflow.py
+++ enrichment-bytewax/dataflow.py
@@ -1,35 +1,88 @@
 import json
 import os
+import orjson
+
+from typing import Dict, Any
 
 from bytewax import operators as op
-from bytewax.connectors.kafka import KafkaSinkMessage, operators as kop
+from bytewax.connectors.kafka import KafkaSinkMessage, KafkaSourceMessage, operators as kop
 from bytewax.dataflow import Dataflow
+from bytewax.connectors.kafka.serde import SchemaDeserializer
 
-# Read environment variables
+from bytewax.testing import TestingSource
+
+from dataclasses import dataclass
+
+@dataclass
+class OrderItemWithContext:
+    id: str
+    item: str
+    createdAt: str
+    
+# TODO: Read environment variables, I ignore these in the code for now but cool kids use them
 orders_topic = os.getenv('ORDERS_TOPIC', 'orders')
 products_topic = os.getenv('PRODUCTS_TOPIC', 'products')
 enriched_order_items_topic = os.getenv('ENRICHED_ORDER_ITEMS_TOPIC', 'enriched-order-items')
-brokers = os.getenv('BROKERS').split(',')
 
+# brokers = os.getenv('BROKERS').split(',')
+brokers = ["kafka:9092"]
 
 def extract_items(x):
     json_value = json.loads(x.value)
 
     result = []
     for item in json_value["items"]:
-        order_item_with_context = {
-            "orderId": json_value["id"],
-            "orderItem": item,
-            "createdAt": json_value["createdAt"]
-        }
-        result.append(KafkaSinkMessage(item["productId"], json.dumps(order_item_with_context)))
+        order_item_with_context = OrderItemWithContext(
+            id=json_value["id"],
+            item=item,
+            createdAt=json_value["createdAt"]
+        )   
+        result.append((item["productId"], order_item_with_context))
     return result
 
+def to_kafka_sink_message(x):
+    order = x[1].get("order")
+    product = x[1].get("product")
+    if order is None or product is None:
+        pass
+    else:
+        result = {
+            "orderId": order.id,
+            "orderItem": order.item,
+            "createdAt": order.createdAt,
+            "product": product.value
+        }
+        return KafkaSinkMessage(x[0], json.dumps(result))
+
+class KeyDeserializer(SchemaDeserializer[bytes, str]):
+    def de(self, obj: bytes) -> str:
+        return str(obj)
+
+
+class JSONDeserializer(SchemaDeserializer[bytes, Dict]):
+    def de(self, obj: bytes) -> Dict[Any, Any]:
+        return orjson.loads(obj)
 
 flow = Dataflow("test")
-in_orders = kop.input("kafka-in-orders", flow, brokers=brokers, topics=[orders_topic])
-in_products = kop.input("kafka-in-products", flow, brokers=brokers, topics=[products_topic])
+in_orders = kop.input("kafka-in-orders", flow, brokers=brokers, topics=['orders'])
+in_products = kop.input("kafka-in-products", flow, brokers=brokers, topics=['products'])
 
-processed = op.flat_map("flat_map", in_orders.oks, extract_items)
-op.inspect("out", processed)
-kop.output("kafka-out", processed, brokers=brokers, topic=enriched_order_items_topic)
+items = op.flat_map("flat_map", in_orders.oks, extract_items)
+
+val_de = JSONDeserializer()
+key_de = KeyDeserializer()
+
+json_products = kop.deserialize(
+    "load_json", in_products.oks, key_deserializer=key_de, val_deserializer=val_de
+)
+
+keyed_products = op.key_on("key_on_identifier", json_products.oks, lambda x: x.value["id"])
+
+# not sure there is a point to use join_named at all given that I have to transform it anyway, consult TopologyProducer.java to see the expected outcome
+joined = op.join_named("join", running=True, product=keyed_products, order=items)
+
+op.inspect("joined", joined)
+res = op.filter_map("transform", joined, to_kafka_sink_message)
+
+op.inspect("out", res)
+kop.output("kafka-out", res, brokers=brokers, topic='enriched-order-items')
